c00cba2faed983db85a0e55f6cb5702ce059a911
David Rizzo <david@r5photography.com>
2023-01-06 13:46:11 -0500

    Created a BMI Calculator

---
+++ Python/BMI Calculator/main.py
@@ -0,0 +1,18 @@
+height = input("enter your height in inches: ")
+weight = input("enter your weight in lbs: ")
+
+#Convert inputs to float
+height_fl = float(height)
+weight_fl = float(weight)
+
+#Convert imperial to metric
+heightm = height_fl * 0.0254
+weightm = weight_fl * 0.45359237
+
+# BMI Calculation
+
+bmi = weightm / heightm ** 2
+
+#Print BMI
+
+print(int(bmi))

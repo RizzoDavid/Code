86640a1833c769ece5233bf5af92f6266afc2a8f
David Rizzo <david@r5photography.com>
2023-01-06 17:21:04 -0500

    Updated BMI Calculator to include if you are overweight, underweight, or a healthy weight.

--- Python/BMI Calculator/main.py
+++ Python/BMI Calculator/main.py
@@ -11,8 +11,18 @@ weightm = weight_fl * 0.45359237
 
 # BMI Calculation
 
-bmi = round(weightm / heightm ** 2)
+bmi = weightm / heightm ** 2
+bmi_round = round(bmi)
 
 #Print BMI
 
-print(int(bmi))
+if bmi <= 18.5:
+    print(f"Your BMI is {bmi_round}, you are underweight")
+elif bmi < 25:
+    print(f"Your BMI is {bmi_round}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi_round}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi_round}, you are obese.")
+else:
+    print(f"Your BMI is {bmi_round}, you are clinically obese.")
